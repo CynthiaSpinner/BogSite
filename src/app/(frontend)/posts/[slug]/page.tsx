0ae9951a144b5e8322fdc87525b9ad3e53d5d6d1
Cynthia <CynthiaSpinner@gmail.com>
2024-11-18 09:53:22 -0600

    re-added block content and rich text

--- src/app/(frontend)/posts/[slug]/page.tsx
+++ src/app/(frontend)/posts/[slug]/page.tsx
@@ -1,33 +1,37 @@
-import { sanityFetch } from '@/sanity/lib/live'
-import { POST_QUERY } from '@/sanity/lib/queries'
-import { notFound } from 'next/navigation'
-import Link from 'next/link'
+import Link from "next/link";
+import Image from "next/image";
+import { PortableText } from "next-sanity";
+import { client } from "@/sanity/lib/client";
+import { POST_QUERY } from "@/sanity/lib/queries";
 import { urlFor } from "@/sanity/lib/image";
+import { components } from "@/sanity/portableTextComponents";
 
-export default async function Page({
-  params,
-}: {
-  params: Promise<{ id: string }>;
-}) {
-  const {data: post} = await sanityFetch({query: POST_QUERY, params: await params})
+type PostIndexProps = { params: { slug: string } };
 
-  if (!post) {
-    notFound()
-  }
+const options = { next: { revalidate: 60 } };
+
+export default async function Page({ params }: PostIndexProps) {
+  const post = await client.fetch(POST_QUERY, params, options);
 
   return (
-    <main className="container mx-auto grid grid-cols-1 gap-6 p-12">
-        {post?.mainImage ?
-        <img
-          className="w-full aspect-[800/300]"
-          src={urlFor(post.mainImage).width(800).height(300).quality(80).auto('format').url()}
+    <main className="container mx-auto grid grid-cols-1 gap-6 py-12">
+      {post?.mainImage ?
+        <Image
+          className="w-full aspect-[800/400]"
+          src={urlFor(post.mainImage).width(800).height(400).quality(80).auto('format').url()}
           alt={post?.mainImage?.alt || ''}
           width="800"
-          height="300"
+          height="400"
         /> : null}
       <h1 className="text-4xl font-bold text-balance">{post?.title}</h1>
+      {post?.body ? (
+        <div className="prose">
+          <PortableText value={post?.body} components={components} />
+        </div>
+      ) : null}
       <hr />
       <Link href="/posts">&larr; Return to index</Link>
     </main>
   );
 }
+
